603de39a8ca764ac2e6d69ad2f9f379435dddcee
Simon N <simon@c-flat.jp>
2023-05-18 00:37:19 +0900

    APIで利用可能なモデルを取得可能に

--- class/openai.php
+++ class/openai.php
@@ -23,7 +23,40 @@ class CravelOpenAI
   static function get_models()
   {
     $config = CravelJson::get_json('config');
-    return $config['models'];
+
+    $enable_models = CravelOpenAI::get_enable_models();
+    $models = array();
+    foreach ($config['models'] as $model_id => $model_detail) {
+      if (in_array($model_id, $enable_models)) {
+        $models[$model_id] = $model_detail;
+      }
+    }
+
+    return $models;
+  }
+
+  static function get_enable_models()
+  {
+    $api_key = CravelChatGptAutoPostAdmin::get_option('openai_api_key');
+    $options = [
+      'http' => [
+        'method' => 'GET',
+        'header' => [
+          'Content-Type: application/json',
+          'Authorization: Bearer ' . $api_key
+        ]
+      ]
+    ];
+    $context = stream_context_create($options);
+    $response = file_get_contents('https://api.openai.com/v1/models', false, $context);
+    $responseData = json_decode($response, true);
+
+    $models = array();
+    // id を配列に追加
+    foreach ($responseData['data'] as $model) {
+      $models[] = $model['id'];
+    }
+    return $models;
   }
 
   static function get_endpoint($model)
